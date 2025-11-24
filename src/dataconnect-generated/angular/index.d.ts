42637760f61b00aebb4a6f511c0a4665c60fd286
William Best <wpbest@gmail.com>
2025-11-24 04:58:29 -0700

    Deleted the dataconnect-generated diretory

--- src/dataconnect-generated/angular/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { ListUsersData } from '../';
-import { ConnectorConfig, DataConnect, QueryRef, QueryPromise} from '@angular/fire/data-connect';
-import { CreateQueryResult} from '@tanstack/angular-query-experimental';
-import { CreateDataConnectQueryResult, CreateDataConnectQueryOptions } from '@tanstack-query-firebase/angular/data-connect';
-import { FirebaseError } from 'firebase/app';
-import { Injector } from '@angular/core';
-
-export type ListUsersOptions = () => Omit<CreateDataConnectQueryOptions<ListUsersData, undefined>, 'queryFn'>;
-export function injectListUsers(options?: ListUsersOptions, injector?: Injector): CreateDataConnectQueryResult<ListUsersData, undefined>;
